d25ee9973253a90feb869a01912b6e3f8c4c18ee
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-16 17:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function insert_Row() {
   let newRow = newTable.insertRow();
   let newCell1 = newRow.insertCell();
   let newCell2 = newRow.insertCell();
-  let newText1 = document.createTextNode('New Cell');
-  let newText2 = document.createTextNode('New Cell');
+  let newText1 = document.createTextNode('New Cell1');
+  let newText2 = document.createTextNode('New Cell2');
 
   newCell1.appendChild(newText1);
   newCell2.appendChild(newText2);
